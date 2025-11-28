8287737ca90afdd1d7c1fa329bb70bff6165e5a1
PushpakB3096 <rtpushpak@gmail.com>
2025-11-28 17:47:27 +0530

    initial commit

---
+++ Pulse.cs
@@ -0,0 +1,81 @@
+﻿using Playnite.SDK;
+using Playnite.SDK.Events;
+using Playnite.SDK.Models;
+using Playnite.SDK.Plugins;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Pulse
+{
+    public class Pulse : GenericPlugin
+    {
+        private static readonly ILogger logger = LogManager.GetLogger();
+
+        private PulseSettingsViewModel settings { get; set; }
+
+        public override Guid Id { get; } = Guid.Parse("d1ac11bf-1668-455f-ad91-6fdb334a54c5");
+
+        public Pulse(IPlayniteAPI api) : base(api)
+        {
+            settings = new PulseSettingsViewModel(this);
+            Properties = new GenericPluginProperties
+            {
+                HasSettings = true
+            };
+        }
+
+        public override void OnGameInstalled(OnGameInstalledEventArgs args)
+        {
+            // Add code to be executed when game is finished installing.
+        }
+
+        public override void OnGameStarted(OnGameStartedEventArgs args)
+        {
+            // Add code to be executed when game is started running.
+        }
+
+        public override void OnGameStarting(OnGameStartingEventArgs args)
+        {
+            // Add code to be executed when game is preparing to be started.
+        }
+
+        public override void OnGameStopped(OnGameStoppedEventArgs args)
+        {
+            // Add code to be executed when game is preparing to be started.
+        }
+
+        public override void OnGameUninstalled(OnGameUninstalledEventArgs args)
+        {
+            // Add code to be executed when game is uninstalled.
+        }
+
+        public override void OnApplicationStarted(OnApplicationStartedEventArgs args)
+        {
+            // Add code to be executed when Playnite is initialized.
+        }
+
+        public override void OnApplicationStopped(OnApplicationStoppedEventArgs args)
+        {
+            // Add code to be executed when Playnite is shutting down.
+        }
+
+        public override void OnLibraryUpdated(OnLibraryUpdatedEventArgs args)
+        {
+            // Add code to be executed when library is updated.
+        }
+
+        public override ISettings GetSettings(bool firstRunSettings)
+        {
+            return settings;
+        }
+
+        public override UserControl GetSettingsView(bool firstRunSettings)
+        {
+            return new PulseSettingsView();
+        }
+    }
+}
